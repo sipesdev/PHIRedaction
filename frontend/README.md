d5afe4421a309b2f614cb12275be5affab63a721
sipesdev <michael@sipes.dev>
2025-04-10 17:19:13 -0700

    Initial commit

---
+++ frontend/README.md
@@ -0,0 +1,20 @@
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
+
+## Learn More
+
+To learn more about Next.js, take a look at the following resources:
+
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+
+You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
---
+++ frontend/app/page.tsx
@@ -0,0 +1,75 @@
+'use client'; // Use client-side rendering
+
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Home() {
+  const [error, setError] = useState<string | null>(null);
+  const [processing, setProcessing] = useState(false);
+
+  // Function to handle file upload
+  const handleFileUpload = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // Prevent default form behavior
+    setError(null); // Reset error state
+    setProcessing(false); // Reset processing state
+    const formData = new FormData(event.currentTarget);
+    
+    try {
+      // Send the file to the backend
+      const response = await fetch("localhost:8000/api/upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      // Handle the response
+      if (response.ok) {
+        const result = await response.json();
+        console.log("File uploaded successfully:", result);
+      } else {
+        console.error("File upload failed:", response.statusText);
+        setError(`File upload failed with status: ${response.status}`);
+      }
+    } catch (ex) {
+      console.error("An unexpected error occured:", ex);
+      setError(`${ex}`);
+    }
+  }
+
+  return (
+    <div className="grid grid-rows-[auto_1fr_auto] items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center mx-auto w-full max-w-4xl">
+        <h1 className="text-[32px] leading-[40px] font-bold">
+          PHI Redaction App
+        </h1>
+        <div className="flex gap-4 items-center flex-col sm:flex-row w-full justify-center">
+          {/* Upload form */}
+          <form
+            onSubmit={handleFileUpload}
+            encType="multipart/form-data"
+            className="flex flex-col gap-4 w-full sm:w-auto">
+            {/* Include file */}
+            <input type="file" name="file" accept=".txt" required className="border p-2 rounded w-full"/>
+
+            {/* Upload button */}
+            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Redact Information
+            </button>
+          </form>
+        </div>
+
+        {/* Error message container */}
+        <div className="min-h-[110px]">
+          {error && (
+            <div className="flex flex-col mt-4 p-4 bg-red-100 text-red-700 border border-red-400 rounded w-full">
+              <p className="font-semibold">An error has occurred!</p>
+              <p>{error}</p>
+            </div>
+          )}
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center mx-auto">
+        Hello Invene! :)
+      </footer>
+    </div>
+  );
+}
